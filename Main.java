d270a06bf8e7a65a827643d8af4516b2a01bd8bb
hiteke <hiteke@mail.ru>
2025-11-01 02:20:05 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,46 @@
-import java.util.*;
-import java.io.*;
-import java.nio.file.*;
+import java.util.Scanner;
 
-class CarTimsortProgramm {
-    private final static List<Car> cars = new ArrayList<>();
-    private final static Scanner scanner = new Scanner(System.in);
+public class CarTimsortProgramm {
+    private final CarDataManager dataManager;
+    private final Scanner scanner;
 
-    private static void displayMenu() {
-        System.out.println("\n" + "=".repeat(60));
-        System.out.println("МЕНЮ:");
-        System.out.println("1. Загрузить данные из файла");
-        System.out.println("2. Сгенерировать случайные данные");
-        System.out.println("3. Ввести данные вручную");
-        System.out.println("4. Показать список машин");
-        System.out.println("5. Отсортировать машины");
-        System.out.println("6. Очистить данные");
-        System.out.println("7. Выйти из программы");
-        System.out.println("=".repeat(60));
+    public CarTimsortProgramm() {
+        this.scanner = new Scanner(System.in);
+        this.dataManager = new CarDataManager(scanner);
     }
+
     public static void main(String[] args) {
+        CarTimsortProgramm program = new CarTimsortProgramm();
+        program.run();
+    }
+
+    public void run() {
         System.out.println("=== ПРОГРАММА СОРТИРОВКИ МАШИН С ПОМОЩЬЮ TIMSORT ===");
         System.out.println("Параметры: мощность, модель, год производства");
 
         boolean running = true;
         while (running) {
             displayMenu();
-            int choice = getValidatedInt("Выберите действие: ", 1, 7);
+            int choice = InputValidator.getValidatedInt(scanner, "Выберите действие: ", 1, 7);
 
             switch (choice) {
                 case 1:
-                    loadFromFile();
+                    dataManager.loadFromFile();
                     break;
                 case 2:
-                    generateRandomData();
+                    dataManager.generateRandomData();
                     break;
                 case 3:
-                    enterManualData();
+                    dataManager.enterManualData();
                     break;
                 case 4:
-                    displayCars();
+                    dataManager.displayCars();
                     break;
                 case 5:
-                    sortCars();
+                    dataManager.sortCars();
                     break;
                 case 6:
-                    clearData();
+                    dataManager.clearData();
                     break;
                 case 7:
                     running = false;
@@ -58,6 +54,19 @@ class CarTimsortProgramm {
             }
         }
         scanner.close();
+    }
+
+    private void displayMenu() {
+        System.out.println("\n" + "=".repeat(60));
+        System.out.println("МЕНЮ:");
+        System.out.println("1. Загрузить данные из файла");
+        System.out.println("2. Сгенерировать случайные данные");
+        System.out.println("3. Ввести данные вручную");
+        System.out.println("4. Показать список машин");
+        System.out.println("5. Отсортировать машины");
+        System.out.println("6. Очистить данные");
+        System.out.println("7. Выйти из программы");
+        System.out.println("=".repeat(60));
     }
 }
 
